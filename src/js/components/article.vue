37445b74a53777be019653388e950dd474e766bb
romgerman <romgerman@users.noreply.github.com>
2019-01-12 17:59:43 +0300

    More stuff

---
+++ src/js/components/article.vue
@@ -0,0 +1,35 @@
+<template>
+<div class="article">
+	<span class="subtitle"><a>ШпильКрафтВерк-Мейстерат Кситилона</a>, 6 ноября 2018, 13:17</span>
+	<h2><a>Почему Гаминатор 18 закончился странно</a></h2>
+	<p>Потому что на первом месте РТС, за которую голосовали люди мало чего понимающие в РТС. За игры конкретного жанра должны голосовать люди, разбирающиеся в этом жанре, а не падкие на графон-музон, при плохо сделанном интерфейсе, который как игроки критиковали, так и разработчик открыто признал (что внушает свою долю уважения к нему, конечно, но правдой быть не перестаёт от этого).</p>
+	<hr>
+	<div class="columns">
+		<div class="column col-lg-auto">
+			<figure class="avatar avatar-sm">
+				<img src="https://picturepan2.github.io/spectre/img/avatar-1.png" alt="...">
+			</figure>
+			Hello
+		</div>
+		<div class="column col-auto">
+			tags
+		</div>
+	</div>
+</div>
+</template>
+
+<style scoped>
+.article {
+	margin-bottom: 26px;
+}
+
+.subtitle {
+	font-size: 0.8em;
+}
+
+hr {
+	border: none;
+	border-top: 1px solid #cccccc;
+}
+
+</style>
--- src/js/index.js
+++ src/js/index.js
@@ -6,9 +6,9 @@ const VueRouter = require('vue-router')
 Vue.use(VueRouter)
 
 const routes = [
-	{ path: '/', component: require('./components/pages/home.vue') },
-	{ path: '/login', component: require('./components/pages/login.vue') },
-	{ path: '/register', component: require('./components/pages/register.vue') },
+	{ path: '/', component: require('./pages/home.vue') },
+	{ path: '/login', component: require('./pages/login.vue') },
+	{ path: '/register', component: require('./pages/register.vue') },
 ]
 
 const router = new VueRouter({ routes })
