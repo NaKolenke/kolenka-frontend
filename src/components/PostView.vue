050206efdc3335ad4bf3869c006cb3602a2405be
romgerman <romgerman@users.noreply.github.com>
2019-05-22 16:19:24 +0300

    Youtube embed replacement with an image

--- src/components/PostView.vue
+++ src/components/PostView.vue
@@ -5,7 +5,7 @@
       {{post.created_date | moment}}
     </span>
     <h2>
-      <router-link :to="{ name: 'post',  params: { post: post.url }}">{{post.title}}</router-link>
+      <router-link :to="{ name: 'post',  params: { post: post.url }}">{{post.title}} <span v-if="post.is_draft" class="label label-primary">черновик</span></router-link>
     </h2>
 
     <div v-if="cut" v-html="post.cut_text"></div>
@@ -43,6 +43,7 @@
 import AvatarView from '@/components/AvatarView'
 import hljs from 'highlight.js/lib/highlight'
 import '@/highlight'
+import createElement from '@/utils/createelement'
 
 export default {
   props: ['post', 'cut'],
@@ -51,9 +52,50 @@ export default {
   },
   mounted() {
     if (this.$el.querySelectorAll) {
-      let targets = this.$el.querySelectorAll('code')
-      targets.forEach(target => {
+      this.wrapCode()
+      this.wrapYoutube()      
+    }
+  },
+  methods: {
+    wrapCode() {
+      let codeTargets = this.$el.querySelectorAll('code')
+      codeTargets.forEach(target => {
         hljs.highlightBlock(target)
+      })
+    },
+    wrapYoutube() {
+      let youtubeTargets = this.$el.querySelectorAll('iframe')
+      youtubeTargets.forEach(target => {
+        let id = target.src.match(/embed\/[a-zA-Z0-9_]+/)[0].substr(6)
+        let image = `http://img.youtube.com/vi/${id}/hqdefault.jpg`
+        
+        let wrapper = createElement('div', {
+          classList: [ 'youtube-video' ]
+        })
+        target.parentNode.insertBefore(wrapper, target)
+        wrapper.appendChild(target)
+        wrapper.removeChild(target)
+
+        let img = createElement('img', {
+          src: image,
+          width: target.width,
+          height: target.height
+        })
+        wrapper.appendChild(img)
+        
+        let span = createElement('span', {
+          classList: [ 'icon-youtube' ]
+        })
+        wrapper.appendChild(span)
+
+        let click = e => {
+          wrapper.appendChild(target)
+          img.style.display = 'none'
+          span.style.display = 'none'
+        }
+
+        img.addEventListener('click', click)
+        span.addEventListener('click', click)
       })
     }
   }
@@ -75,3 +117,26 @@ export default {
   margin-top: 12px;
 }
 </style>
+
+<style lang="scss">
+.youtube-video {
+  position: relative;
+}
+
+.youtube-video img {
+  position: relative;
+  cursor: pointer;
+}
+
+.youtube-video span {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  color: #fff;
+  transform: translate(-50%, -50%);
+  font-size: 64px;
+  text-shadow: 0 10px 50px #000;
+  cursor: pointer;
+}
+</style>
+
