ebe906729a2609e68e665b3034e8b542cefcd33f
romgerman <romgerman@users.noreply.github.com>
2019-06-12 23:37:15 +0300

    Fix for Edge

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,23 @@
 module.exports = {
   presets: [
     [
+      '@babel/preset-env',
+      {
+        targets: {
+          node: "6.10",
+          esmodules: true
+        }
+      }
+    ],
+    [
       '@vue/app',
-      { useBuiltIns: "entry" }
+      { 
+        useBuiltIns: "entry"
+      }
     ]
-  ]
+  ],
+  plugins: [ 
+    "@babel/plugin-proposal-object-rest-spread" 
+  ],
+  sourceType: "unambiguous"
 }
