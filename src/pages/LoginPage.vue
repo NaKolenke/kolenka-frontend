33b33bf315b6b2898b15615580a23ef5077a4ef8
romgerman <romgerman@users.noreply.github.com>
2019-06-18 14:49:40 +0300

    Password restore page

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,12 +1,13 @@
 <template>
-  <div class="container">
+  <div class="container" style="margin-top: 20%;">
     <div class="columns">
       <div id="login" class="column col-6 col-mx-auto col-md-12">
         <h1>Войти</h1>
-        <form method="POST" @submit.prevent="login" :class="{ 'has-error': !validation.username.success || !validation.password.success }">
+        <form method="POST" @submit.prevent="login" :class="{ 'has-error': !isValid && validation.showErrors }">
           <div class="form-group">
             <label class="form-label" for="username">Имя пользователя</label>
             <input 
+              type="text"
               class="form-input" 
               v-model="username"
               v-validate="validation.username"
@@ -35,7 +36,11 @@
         </form>
         
         <br>
-        <p>Еще нет учетной записи? <router-link to="/register">Зарегистрироваться</router-link></p>
+        <p>
+          Еще нет учетной записи? <router-link to="/register">Зарегистрироваться</router-link>
+          <br>
+          Забыли пароль? <router-link to="/restore">Восстановить</router-link>
+        </p>
       </div>
     </div>
   </div>
@@ -68,10 +73,9 @@ export default {
   },
   methods: {
     login () {
-      if (!this.validation.username.success ||
-          !this.validation.password.success) {
-            this.validation.showErrors = true
-            return
+      if (!this.isValid) {
+        this.validation.showErrors = true
+        return
       } else {
         this.validation.showErrors = false
       }
@@ -86,6 +90,12 @@ export default {
       .catch(() => {
         this.$toast.error('Ошибка авторизации')
       })
+    }
+  },
+  computed: {
+    isValid() {
+      return this.validation.username.success &&
+             this.validation.password.success
     }
   }
 }
