b0941cd281dd9d7949f1790877ad81d1632d0e29
romgerman <romgerman@users.noreply.github.com>
2019-06-20 19:57:38 +0300

    Search 2

--- src/pages/SearchPage.vue
+++ src/pages/SearchPage.vue
@@ -1,48 +1,93 @@
 <template>
   <div>
     <h2>Поиск</h2>
-    <form @submit.prevent="send">
-      <input class="form-input" type="text" placeholder="Введите запрос" v-model="model.query" />
-      <div class="columns mt-2">
-        <div class="column col-lg-auto">
-          <div class="form-group">
-            <label class="form-radio form-inline">
-              <input type="radio" name="type" value="post" v-model="model.type">
-              <i class="form-icon"></i> Посты
-            </label>
-            <label class="form-radio form-inline">
-              <input type="radio" name="type" value="blog" v-model="model.type">
-              <i class="form-icon"></i> Блоги
-            </label>
-            <label class="form-radio form-inline">
-              <input type="radio" name="type" value="user" v-model="model.type">
-              <i class="form-icon"></i> Пользователи
-            </label>
+    <form @submit.prevent="send" class="panel mb-2">
+      <div class="panel-header">
+        <input class="form-input" type="text" placeholder="Введите запрос" v-model="model.query" />
+      </div>
+      
+      <div class="panel-body mb-2">
+        <div class="columns mt-2">
+          <div class="column col-lg-auto">
+            <div class="form-group">
+              <label class="form-radio form-inline">
+                <input type="radio" name="type" value="post" v-model="model.type">
+                <i class="form-icon"></i> Посты
+              </label>
+              <label class="form-radio form-inline">
+                <input type="radio" name="type" value="blog" v-model="model.type">
+                <i class="form-icon"></i> Блоги
+              </label>
+              <label class="form-radio form-inline">
+                <input type="radio" name="type" value="user" v-model="model.type">
+                <i class="form-icon"></i> Пользователи
+              </label>
+            </div>
           </div>
+          <input class="btn btn-primary column col-2 mx-2" type="submit" value="Найти" />
         </div>
-        <input class="btn btn-primary column col-2 mx-2" type="submit" value="Найти" />
       </div>
     </form>
 
     <div v-if="model.type === 'full' && queryAvailable" class="columns col-12 mt-2">
       <div class="column col-6">
         <h4>Пользователи</h4>
-        <ul style="list-style:none;margin-left:0">
-          <li v-for="item in users" :key="item.id">
-            <avatar :user="item" size="sm"></avatar><router-link :to="{ name: 'profile', params: { user: item.username } }"> {{ item.name || item.username }}</router-link>
-          </li>
-        </ul>
-        <a v-if="users.length > 5" href="#">Найти еще пользователей</a>
+        
+        <div v-if="users.length > 0">
+          <ul style="list-style:none;margin-left:2px">
+            <li v-for="item in users" :key="item.id">
+              <avatar :user="item" size="sm"></avatar><router-link :to="{ name: 'profile', params: { user: item.username } }"> {{ item.name || item.username }}</router-link>
+            </li>
+          </ul>
+          <a v-if="users.length > 5" href="#">Найти еще пользователей</a>
+        </div>
+
+        <div v-else class="empty">
+          <p class="empty-title h5">Пользователей не найдено</p>
+        </div>
       </div>
       <div class="column col-6">
         <h4>Блоги</h4>
-        <blog-card-small v-for="item in blogs.slice(0, 2)" :key="item.id" :blog="item"></blog-card-small>
-        <a v-if="blogs.length > 2" href="#">Найти еще блоги</a>
+
+        <div v-if="blogs.length > 0">
+          <blog-card-small v-for="item in blogs.slice(0, 2)" :key="item.id" :blog="item"></blog-card-small>
+          <a v-if="blogs.length > 2" href="#">Найти еще блоги</a>
+        </div>
+
+        <div v-else class="empty">
+          <p class="empty-title h5">Блогов не найдено</p>
+        </div>
       </div>
     </div>
 
-    <h4 v-if="queryAvailable">Посты</h4>
-    <post-view v-for="post in posts" :key="post.id" :post="post" :cut="true"></post-view>
+    
+    <div v-if=" queryAvailable && model.type === 'post' || model.type === 'full'">
+      <h4 v-if="queryAvailable">Посты</h4>
+      <post-view v-for="post in posts" :key="post.id" :post="post" :cut="true"></post-view>
+
+      <div v-if="queryAvailable && posts.length === 0" class="empty">
+        <p class="empty-title h5">Записей не найдено</p>
+      </div>
+    </div>
+
+    <div v-if="queryAvailable && model.type === 'blog'">
+      <h4 v-if="queryAvailable">Блоги</h4>
+      <div class="columns">
+        <div v-for="item in blogs" :key="item.id" class="column col-4">
+          <blog-card-small :blog="item"></blog-card-small>
+        </div>
+      </div>
+    </div>
+
+    <div v-if="queryAvailable && model.type === 'user'">
+      <h4 v-if="queryAvailable">Пользователи</h4>
+      <div class="columns">
+        <div v-for="item in users" :key="item.id" class="column col-3">
+          <avatar :user="item" size="sm"></avatar><router-link :to="{ name: 'profile', params: { user: item.username } }"> {{ item.name || item.username }}</router-link>
+        </div>
+      </div>
+    </div>
+
     <pagination-view v-if="queryAvailable" :page="page" :page-count="pageCount"></pagination-view>
   </div>
 </template>
@@ -69,18 +114,46 @@ export default {
     }
   },
   mounted() {
-
+    if (this.$route.query)
+      this.refresh(this.$route)
+  },
+  beforeRouteUpdate (to, from, next) {
+    this.refresh(to)
   },
   methods: {
     send() {
-      if (this.model.type === 'full') {
-        //this.refreshUsers()
-        //this.refreshBlogs()
-        //this.refreshPosts()
-        //this.queryAvailable = true
+      this.$router.push({ name: 'search', query: { type: this.model.type, q: this.model.query } })
+    },
+    refresh(route) {
+      this.model.query = route.query.q || ''
+      const type = route.query.type
+
+      if (this.model.query.length === 0)
+        return
+    
+      if (type === 'full') {
+        this.fullRefresh()
+        this.queryAvailable = true
+      } else if (type === 'post') {
+        this.refreshPosts()
+        this.queryAvailable = true
+      } else if (type === 'blog') {
+        this.refreshBlogs()
+        this.queryAvailable = true
+      } else if (type === 'user') {
+        this.refreshUsers()
+        this.queryAvailable = true
+      } else {
+        this.$router.replace('/search')
+        return
       }
 
-      this.$router.push({ name: 'search', query: { type: this.model.type, q: this.model.query } })
+      this.model.type = type
+    },
+    fullRefresh() {
+      this.refreshUsers()
+      this.refreshBlogs()
+      this.refreshPosts()
     },
     refreshUsers() {
       this.$search.findUsers(this.model.query, { page: this.page }).then(res => {
