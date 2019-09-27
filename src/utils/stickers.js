a3689533e56786300eef874716f72b0ba593527e
Pavel <useless.dinosaur@veloc1.me>
2019-09-27 06:13:30 +0300

    Stickers name fix. Added 0-9 to stickers regexp

--- src/utils/stickers.js
+++ src/utils/stickers.js
@@ -1,4 +1,4 @@
 export default function processStickers (html) {
-  let r = html.replace(/:([a-z]*?):/gm, `<img src="${process.env.VUE_APP_API_URL}stickers/$1"/>`)
+  let r = html.replace(/:([a-z0-9]*?):/gm, `<img src="${process.env.VUE_APP_API_URL}stickers/$1"/>`)
   return r
 }
